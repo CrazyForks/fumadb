bfc26ab06ca78cc533fc4cedc0b75ec2554afeeb
Fuma Nama <god63820869@gmail.com>
2026-01-04 22:58:01 +0800

    bump deps

--- apps/docs/app/docs/[[...slug]]/page.tsx
+++ apps/docs/app/docs/[[...slug]]/page.tsx
@@ -4,7 +4,7 @@ import {
   DocsBody,
   DocsDescription,
   DocsTitle,
-} from "fumadocs-ui/page";
+} from "fumadocs-ui/layouts/docs/page";
 import { notFound } from "next/navigation";
 import defaultMdxComponents, { createRelativeLink } from "fumadocs-ui/mdx";
 
